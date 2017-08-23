1e4f71cd48b6f1043c401510331b938b383209ad
Shujon Mollah <mollah@gmail.com>
2017-08-23 00:34:14 -0400

    Download zip file on button press

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { FileSystem } from 'expo'
 
 export default class App extends React.Component {
+  async downloadFile() {
+    const localUri = `${FileSystem.documentDirectory}files.zip`
+    console.log('localUri', localUri)
+    const remoteUrl = 'https://raw.githubusercontent.com/smoll/crna-zipfile/master/remote/files.zip'
+    const res = await FileSystem.downloadAsync(remoteUrl, localUri)
+    console.log('Finished downloading to ', res.uri)
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+
+        <Button
+          onPress={this.downloadFile}
+          title="Download Zip"
+          color="#841584"
+        />
       </View>
-    );
+    )
   }
 }
 
@@ -20,4 +35,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
